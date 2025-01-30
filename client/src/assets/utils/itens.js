0f6ce7c618ce75ec61658499865d2ef76f68da52
KauaLQ <limakaua610@gmail.com>
2025-01-30 19:36:56 -0300

    Adicionando todas as fotos dos sapatos

--- client/src/assets/utils/itens.js
+++ client/src/assets/utils/itens.js
@@ -6,19 +6,89 @@ import endo_4 from "../img/endo_4.webp";
 import endo_main from "../img/endo_main.webp";
 
 import addidas_cp from "../img/adidas-cp.png";
+import addidas_cp1 from "../img/adidas-cp1.webp";
+import addidas_cp2 from "../img/adidas-cp2.webp";
+import addidas_cp3 from "../img/adidas-cp3.webp";
+import addidas_cp4 from "../img/adidas-cp4.webp";
+import addidas_cp_main from "../img/adidas-cp_main.webp";
+
 import olympikus_vertical from "../img/olymoikus_vertical.png";
+import olympikus_vertical_1 from "../img/olympikus_vertical_1.webp";
+import olympikus_vertical_2 from "../img/olympikus_vertical_2.webp";
+import olympikus_vertical_3 from "../img/olympikus_vertical_3.webp";
+import olympikus_vertical_4 from "../img/olympikus_vertical_4.webp";
+
 import fila_street from "../img/fila_street.png";
+import fila_street_1 from "../img/fila_street_1.webp";
+import fila_street_2 from "../img/fila_street_2.webp";
+import fila_street_3 from "../img/fila_street_3.webp";
+import fila_street_4 from "../img/fila_street_4.webp";
+
 import fila_tormo from "../img/fila_tormo.png";
+import fila_tormo_1 from "../img/fila_tormo_1.webp";
+import fila_tormo_2 from "../img/fila_tormo_2.webp";
+import fila_tormo_3 from "../img/fila_tormo_3.webp";
+import fila_tormo_4 from "../img/fila_tormo_4.webp";
+
 import grand_court from "../img/grand_court.png";
+import grand_court1 from "../img/grand_court1.avif";
+import grand_court2 from "../img/grand_court2.avif";
+import grand_court3 from "../img/grand_court3.avif";
+import grand_court4 from "../img/grand_court4.avif";
+
 import new_balance from "../img/new_balance.png";
+import new_balance1 from "../img/new_balance1.webp";
+import new_balance2 from "../img/new_balance2.webp";
+import new_balance3 from "../img/new_balance3.webp";
+import new_balance4 from "../img/new_balance4.webp";
+
 import nike_air_max from "../img/nike_air_max.png";
+import nike_air_max1 from "../img/nike_air_max1.webp";
+import nike_air_max2 from "../img/nike_air_max2.webp";
+import nike_air_max3 from "../img/nike_air_max3.webp";
+import nike_air_max4 from "../img/nike_air_max4.webp";
+
 import nike_jordan from "../img/nike-jordan.png";
+import nike_jordan1 from "../img/nike_jordan1.webp";
+import nike_jordan2 from "../img/nike_jordan2.webp";
+import nike_jordan3 from "../img/nike_jordan3.webp";
+import nike_jordan4 from "../img/nike_jordan4.webp";
+
 import olympikus_aqua from "../img/olympikus_aqua.png";
+import olympikus_aqua1 from "../img/olympikus_aqua1.webp";
+import olympikus_aqua2 from "../img/olympikus_aqua2.webp";
+import olympikus_aqua3 from "../img/olympikus_aqua3.webp";
+import olympikus_aqua4 from "../img/olympikus_aqua4.webp";
+
 import olympikus_difuse from "../img/olympikus_difuse.png";
+import olympikus_difuse1 from "../img/olympikus_difuse1.webp";
+import olympikus_difuse2 from "../img/olympikus_difuse2.webp";
+import olympikus_difuse3 from "../img/olympikus_difuse3.webp";
+import olympikus_difuse4 from "../img/olympikus_difuse4.webp";
+
 import poly_joy from "../img/poly_joy.png";
+import poly_joy1 from "../img/poly_joy1.webp";
+import poly_joy2 from "../img/poly_joy2.webp";
+import poly_joy3 from "../img/poly_joy3.webp";
+import poly_joy4 from "../img/poly_joy4.webp";
+
 import vertus from "../img/VertusCasual.png";
+import vertus1 from "../img/Vertus1.webp";
+import vertus2 from "../img/Vertus2.webp";
+import vertus3 from "../img/Vertus3.webp";
+import vertus4 from "../img/Vertus4.webp";
+
 import kidy from "../img/KidyInfantil.png";
+import kidy1 from "../img/Kidy1.webp";
+import kidy2 from "../img/Kidy2.webp";
+import kidy3 from "../img/Kidy3.webp";
+import kidy4 from "../img/Kidy4.webp";
+
 import dnaflex from "../img/DnaFlex.png";
+import dnaflex1 from "../img/DnaFlex1.webp";
+import dnaflex2 from "../img/DnaFlex2.webp";
+import dnaflex3 from "../img/DnaFlex3.webp";
+import dnaflex4 from "../img/DnaFlex4.webp";
 
 const itens = [
   {
@@ -35,7 +105,7 @@ const itens = [
     preco_small: "99",
     link: addidas_cp,
     id: 2,
-    price: "899.99",
+    price: "899.99", photo_main: addidas_cp_main, photo_one: addidas_cp1, photo_two: addidas_cp2, photo_three: addidas_cp3, photo_four: addidas_cp4,
   },
   {
     nome: "Olympikus Vertical",
@@ -43,13 +113,13 @@ const itens = [
     preco_small: "83",
     link: olympikus_vertical,
     id: 3,
-    price: "307.83",
+    price: "307.83", photo_main: olympikus_vertical, photo_one: olympikus_vertical_1, photo_two: olympikus_vertical_2, photo_three: olympikus_vertical_3, photo_four: olympikus_vertical_4,
   },
   {
     nome: "Fila Street Fit",
     preco_big: "R$ 360.",
     preco_small: "00",
-    link: fila_street,
+    link: fila_street, photo_main: fila_street, photo_one: fila_street_1, photo_two: fila_street_2, photo_three: fila_street_3, photo_four: fila_street_4,
     id: 4,
     price: "360.00",
   },
@@ -59,7 +129,7 @@ const itens = [
     preco_small: "99",
     link: fila_tormo,
     id: 5,
-    price: "369.99",
+    price: "369.99", photo_main: fila_tormo, photo_one: fila_tormo_1, photo_two: fila_tormo_2, photo_three: fila_tormo_3, photo_four: fila_tormo_4,
   },
   {
     nome: "grand Court 2.0",
@@ -67,7 +137,7 @@ const itens = [
     preco_small: "99",
     link: grand_court,
     id: 6,
-    price: "219.99",
+    price: "219.99", photo_main: grand_court, photo_one: grand_court1, photo_two: grand_court2, photo_three: grand_court3, photo_four: grand_court4,
   },
   {
     nome: "New Balance 480",
@@ -75,7 +145,7 @@ const itens = [
     preco_small: "99",
     link: new_balance,
     id: 7,
-    price: "499.99",
+    price: "499.99", photo_main: new_balance, photo_one: new_balance1, photo_two: new_balance2, photo_three: new_balance3, photo_four: new_balance4,
   },
   {
     nome: "Nike Air Max Intrlk",
@@ -83,7 +153,7 @@ const itens = [
     preco_small: "99",
     link: nike_air_max,
     id: 8,
-    price: "599.99",
+    price: "599.99", photo_main: nike_air_max, photo_one: nike_air_max1, photo_two: nike_air_max2, photo_three: nike_air_max3, photo_four: nike_air_max4,
   },
   {
     nome: "Jordan one take 4",
@@ -91,7 +161,7 @@ const itens = [
     preco_small: "99",
     link: nike_jordan,
     id: 9,
-    price: "669.99",
+    price: "669.99", photo_main: nike_jordan, photo_one: nike_jordan1, photo_two: nike_jordan2, photo_three: nike_jordan3, photo_four: nike_jordan4,
   },
   {
     nome: "Olympikus Acqua",
@@ -99,7 +169,7 @@ const itens = [
     preco_small: "00",
     link: olympikus_aqua,
     id: 10,
-    price: "189.00",
+    price: "189.00", photo_main: olympikus_aqua, photo_one: olympikus_aqua1, photo_two: olympikus_aqua2, photo_three: olympikus_aqua3, photo_four: olympikus_aqua4,
   },
   {
     nome: "Olympikus Difuse 4",
@@ -107,7 +177,7 @@ const itens = [
     preco_small: "99",
     link: olympikus_difuse,
     id: 11,
-    price: "170.99",
+    price: "170.99", photo_main: olympikus_difuse, photo_one: olympikus_difuse1, photo_two: olympikus_difuse2, photo_three: olympikus_difuse3, photo_four: olympikus_difuse4,
   },
   {
     nome: "Polo joy casual",
@@ -115,7 +185,7 @@ const itens = [
     preco_small: "90",
     link: poly_joy,
     id: 12,
-    price: "99.90",
+    price: "99.90", photo_main: poly_joy, photo_one: poly_joy1, photo_two: poly_joy2, photo_three: poly_joy3, photo_four: poly_joy4,
   },
   {
     nome: "Vertus Casual",
@@ -123,7 +193,7 @@ const itens = [
     preco_small: "90",
     link: vertus,
     id: 13,
-    price: "79.90",
+    price: "79.90", photo_main: vertus, photo_one: vertus1, photo_two: vertus2, photo_three: vertus3, photo_four: vertus4,
   },
   {
     nome: "Kidy Infantil Preto-Pink",
@@ -131,7 +201,7 @@ const itens = [
     preco_small: "99",
     link: kidy,
     id: 14,
-    price: "84.99",
+    price: "84.99", photo_main: kidy, photo_one: kidy1, photo_two: kidy2, photo_three: kidy3, photo_four: kidy4,
   },
   {
     nome: "Ortopé DNA Flex",
@@ -139,7 +209,7 @@ const itens = [
     preco_small: "90",
     link: dnaflex,
     id: 15,
-    price: "99.90",
+    price: "99.90", photo_main: dnaflex, photo_one: dnaflex1, photo_two: dnaflex2, photo_three: dnaflex3, photo_four: dnaflex4,
   }
 ];
 
