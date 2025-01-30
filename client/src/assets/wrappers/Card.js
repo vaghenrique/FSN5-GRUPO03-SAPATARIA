aee128e4ddb18056ed3c8e876242bf60c66f286e
KauaLQ <limakaua610@gmail.com>
2025-01-29 23:45:26 -0300

    Modal quase finalizado

--- client/src/assets/wrappers/Card.js
+++ client/src/assets/wrappers/Card.js
@@ -119,5 +119,184 @@ export const Wrapper = styled.div`
     color: #0d6efd;
     transition: 0.5s;
   }
+
+/* =============================================================== */
+
+.modal-window{
+    height: 100vh;
+    width: 100vw;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background-color:rgba(0, 0, 0, 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 1000;
+}
+
+.modal{
+    width: 60%;
+    height: 80%;
+    min-width: 300px;
+    min-height: 300px;
+    background-color: rgb(129, 179, 253);
+    padding: 50px;
+    border-radius: 20px;
+    position: relative;
+    z-index: 10000;
+    animation: fade-in 0.5s;
+    display: block;
+}
+
+.close-button{
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    width: 30px;
+    height: 30px;
+    border-radius: 50%;
+    border: 0;
+    background-color: #0D6EFD;
+    color: #fff;
+    font-size: 20px;
+    cursor: pointer;
+}
+
+@keyframes fade-in{
+    from{
+        opacity: 0;
+        transform: translate3d(0, -20px, -5px);
+    }
+    to{
+        opacity: 1;
+        transform: translate3d(0,0,0);
+    }
+}
+
+/* ================================================================================== */
+
+.row{
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    justify-content: space-around;
+}
+
+.col-2{
+    flex-basis: 50%;
+    min-width: 300px;
+}
+
+.col-2 h1{
+    font-size: 50px;
+    line-height: 60px;
+    margin: 25px 0;
+}
+
+.btn{
+    display: inline-block;
+    background: #0D6EFD;
+    color: #fff;
+    cursor: pointer;
+    padding:8px 30px;
+    margin: 30px 0px;
+    border-radius: 30px;
+    transition: background 0.5s;
+}
+
+.btn:hover{
+    background:rgb(14, 69, 153);
+}
+
+.small-container{
+    max-width: 1080px;
+    margin: auto;
+    padding-left: 25px;
+    padding-right: 25px;
+}
+
+h4{
+    font-weight: normal;
+}
+
+.col-4 p{
+    font-size: 14px;
+}
+
+.col-4:hover{
+    transform:translateY(-5px);
+}
+
+.single-product{
+    margin-top: 0px;
+}
+.single-product .col-2 img{
+    padding:0;
+}
+.single-product .col-2{
+    padding:20px;
+}
+.single-product h4{
+    margin: 20px 0;
+    font-size: 22px;
+    font-weight: bold;
+}
+.single-product select{
+    display: block;
+    padding: 10px;
+    margin-top: 20px;
+}
+.single-product input{
+    width: 50px;
+    height: 40px;
+    padding-left: 10px;
+    font-size: 20px;
+    margin-right: 10px;
+    border: 1px solid #0D6EFD;
+    color: #555;
+}
+
+input:focus{
+    outline: none;
+}
+
+.small-img-row{
+   display:flex;
+    justify-content: space-between;
+}
+.small-img-col{
+    flex-basis: 24%;
+    cursor: pointer;
+}
+
+.small-img{
+    width: 100%;
+}
+
+#productImg{
+    width: 100%;
+}
+
+@media only screen and (max-width:600px){
+    .row{
+        text-align: center;
+    }
+    .col-2{
+        flex-basis: 100%;
+    }
+    .single-product .row{
+        text-align: left;
+    }
+    .single-product .col-2{
+        padding: 20px 0;
+    }
+    .single-product h1{
+        font-size: 20px;
+        line-height: 32px;
+    }
+    
+
+}
 `;
 
--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,9 +2,15 @@ import { Wrapper } from "../assets/wrappers/Card";
 import { MdAddShoppingCart } from "react-icons/md";
 import { useDispatch } from "react-redux";
 import { addItem } from "../store/cartSlice";
+import { useState } from "react";
 
-const Card = ({ nome, preco_big, preco_small, link, id, price }) => {
+const Card = ({ nome, preco_big, preco_small, link, id, price, photo_one, photo_two, photo_three, photo_four}) => {
   const dispatch = useDispatch();
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal)
+  }
 
   const handleAddToCart = () => {
     const produto = {
@@ -32,11 +38,60 @@ const Card = ({ nome, preco_big, preco_small, link, id, price }) => {
             {preco_big}
             <span>{preco_small}</span>
           </h2>
-          <a href="#" className="buy">
+          <a className="buy" onClick={toggleModal}>
             Mais Detalhes
           </a>
         </div>
       </div>
+
+      {modal && (
+        <div className='modal-window' id='modal-window'>
+            <div className='modal' id="modal">
+                <button className='close-button' id='close-button' onClick={toggleModal}>X</button>
+                <div className="small-container single-product">
+                  <div className="row">
+                    <div className="col-2">
+                      <img src={link} alt={nome} id="productImg"/>
+
+                      <div className="small-img-row">
+                        <div className="small-img-col">
+                          <img src={photo_one} alt={nome} className="small-img"/>
+                        </div>
+                        <div className="small-img-col">
+                          <img src={photo_two} alt={nome} className="small-img"/>
+                        </div>
+                        <div className="small-img-col">
+                          <img src={photo_three} alt={nome} className="small-img"/>
+                        </div>
+                        <div className="small-img-col">
+                          <img src={photo_four} alt={nome} className="small-img"/>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="col-2">
+                    <p>Início / Produtos</p>
+                        <h1>{nome}</h1>
+                        <h4>R$ {price}</h4>
+                        <select>
+                            <option>Escolha o tamanho</option>
+                            <option>37</option>
+                            <option>38</option>
+                            <option>39</option>
+                            <option>40</option>
+                            <option>41</option>
+                            <option>42</option>
+                            <option>43</option>
+                        </select>
+                        <input type="number" value="1" min="1" max="10"/>
+                        <a class="btn">Comprar</a>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+      )}
+
     </Wrapper>
   );
 };
